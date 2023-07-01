87888f8b6de465ab99b6e7ca461ae07d6a2eda1a
Y <hwiyul_g@daum.net>
2023-07-02 00:30:10 +0900

    Add : Profile Activity (UI)

---
+++ app/src/main/java/com/example/select_img_from_gallery_android/View/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.select_img_from_gallery_android.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.select_img_from_gallery_android.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
