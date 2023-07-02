0fbca4df632710df0ff83993816bc6288c997a4a
Y <hwiyul_g@daum.net>
2023-07-02 23:46:30 +0900

    Add : TabLayer 적용 및 ViewPager와 연결

--- app/src/main/java/com/example/select_img_from_gallery_android/MainActivity.kt
+++ app/src/main/java/com/example/select_img_from_gallery_android/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.select_img_from_gallery_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.select_img_from_gallery_android.View.adapters.ViewPagerAdapter
+import com.example.select_img_from_gallery_android.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    private val tabTitleArray = arrayOf("","","")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        viewPager.adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tabLayout, viewPager){ tab, position ->
+            tab.text = tabTitleArray[position]
+        }.attach()
+
+
+
     }
 }
