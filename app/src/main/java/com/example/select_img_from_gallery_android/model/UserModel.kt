e51e7859b34ecc58ca9cbfcef64f3912fb361925
Y <hwiyul_g@daum.net>
2023-07-02 14:18:23 +0900

    Add : UserModel data class 추가

---
+++ app/src/main/java/com/example/select_img_from_gallery_android/model/UserModel.kt
@@ -0,0 +1,8 @@
+package com.example.select_img_from_gallery_android.model
+
+data class UserModel(
+    val uid : String,
+    val name : String,
+    val number : String,
+    val imgUrl : String
+)
