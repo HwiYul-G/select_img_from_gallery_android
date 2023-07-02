a702631c23c9f0868a8389c3b04af0a442ab742a
Y <hwiyul_g@daum.net>
2023-07-03 00:37:20 +0900

    Add : NumberActivity.kt에서 사용자의 전화번호를 입력받아서 continue 버튼 클릭시 intent트를 OTP 인증 절차를 거치는 Activity로 넘김! 이 때 왜 finish를 안했을까? 의문 생각해보기

--- app/src/main/java/com/example/select_img_from_gallery_android/View/activities/NumberActivity.kt
+++ app/src/main/java/com/example/select_img_from_gallery_android/View/activities/NumberActivity.kt
@@ -1,13 +1,34 @@
 package com.example.select_img_from_gallery_android.View.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.select_img_from_gallery_android.R
+import com.example.select_img_from_gallery_android.databinding.ActivityNumberBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class NumberActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityNumberBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_number)
+        binding = ActivityNumberBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.setOnClickListener {
+            if(binding.phoneNumber.text!!.isEmpty()){
+                Toast.makeText(this, "Please enter your phone number", Toast.LENGTH_SHORT).show()
+            }else{
+                var intent = Intent(this, OTPActivity::class.java)
+                // key : number / value : phoneNumber로 해서 다음 activity에서 받음
+                intent.putExtra("number", binding.phoneNumber.text.toString())
+                startActivity(intent)
+                // 여기선 finish를 안함? 왜?
+            }
+        }
+
     }
 }
